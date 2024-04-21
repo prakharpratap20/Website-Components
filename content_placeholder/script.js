19c81e1599c078483c00ea3bd0f1eff70aae88cc
Prakhar Pratap <prakharpratap2001@gmail.com>
2024-04-21 23:14:00 +0530

    interesting

--- content_placeholder/script.js
+++ content_placeholder/script.js
@@ -5,10 +5,22 @@ const profile_img = document.getElementById("profile_img");
 const name = document.getElementById("name");
 const date = document.getElementById("date");
 
-const animated_bg = document.querySelectorAll(".animated-bg");
+const animated_bgs = document.querySelectorAll(".animated-bg");
 const animated_bg_texts = document.querySelectorAll(".animated-bg-text");
+
+setTimeout(getData, 2000);
 
 function getData() {
   header.innerHTML =
     '<img src="https://picsum.photos/id/1/2000/3000" alt="" />';
+  title.innerHTML = "Lorem ipsum dolor sit amet";
+  excerpt.innerHTML =
+    "Lorem impsum dolor sit amet consecutur adipsicing elit. Dolore,perferndis.";
+  profile_img.innerHTML =
+    '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />';
+  name.innerHTML = "Jon Doe";
+  date.innerHTML = "Oct 10, 2024";
+
+  animated_bgs.forEach((bg) => bg.classList.remove("animated-bg"));
+  animated_bg_texts.forEach((bg) => bg.classList.remove("animated-bg-text"));
 }
